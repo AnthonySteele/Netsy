a004403c0ed54deff1d6285c032e5418390b90d5
AnthonySteele <Anthony@PlasticAvalon.org>
2009-07-13 13:48:35 +0100

    added silverlight test control and test project

---
+++ Silverlight.Netsy.TestControl/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainPage.xaml.cs" company="AFS">
+// Copyright (c) AFS. All rights reserved.
+// </copyright>
+//----------------------------------------------------------------------- 
+namespace Silverlight.Netsy.TestControl
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Main page class
+    /// </summary>
+    public partial class MainPage : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainPage class 
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Silverlight.Netsy.TestControl/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Silverlight.Netsy.TestControl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Silverlight.Netsy.TestControl")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eef1f8e6-1379-4742-a80f-aa80424cac4f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
