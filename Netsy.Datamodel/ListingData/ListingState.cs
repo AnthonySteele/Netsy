b2d8e15afcc9b98e53c09bf5b6bf18562bbcc2c2
AnthonySteele <Anthony@PlasticAvalon.org>
2009-08-01 21:49:06 +0100

    Started on Listing data scructure

---
+++ Netsy.Datamodel/ListingData/ListingState.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ListingState.cs" company="AFS">
+// Copyright (c) AFS. All rights reserved.
+// </copyright>
+//----------------------------------------------------------------------- 
+
+namespace Netsy.DataModel.ListingData
+{
+    public enum ListingState
+    {
+        /// <summary>
+        /// Unknown default value
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// The listing is active
+        /// </summary>
+        Active, 
+
+        /// <summary>
+        /// The listing is removed
+        /// </summary>
+        Removed, 
+
+        /// <summary>
+        /// The listing is sold out
+        /// </summary>
+        SoldOut, 
+
+        /// <summary>
+        /// The listing is expired
+        /// </summary>
+        Expired, 
+
+        /// <summary>
+        /// The listing is voodoo, man
+        /// </summary>
+        Alchemy
+    }
+}
