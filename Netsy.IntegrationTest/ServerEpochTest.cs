21c5588f7df904ed8c8e3ddb06fc7c59b45048b4
AnthonySteele <Anthony@PlasticAvalon.org>
2009-08-12 22:31:08 +0100

    server epoch integration tests

--- Netsy.IntegrationTest/ServerEpochTest.cs
+++ Netsy.IntegrationTest/ServerEpochTest.cs
@@ -1,6 +1,6 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="PingTest.cs" company="AFS">
-//  This source code is part of Netsy http://github.com/AnthonySteele/Netsy/
+// <copyright file="ServerEpochTest.cs" company="AFS">
+//  This source code is part ServerEpochTest Netsy http://github.com/AnthonySteele/Netsy/
 //  and is made available under the terms of the Microsoft Public License (Ms-PL)
 //  http://www.opensource.org/licenses/ms-pl.html
 // </copyright>
@@ -21,20 +21,20 @@ namespace Netsy.IntegrationTest
     /// Test the ping funcion on the server service
     /// </summary>
     [TestClass]
-    public class PingTest
+    public class ServerEpochTest
     {
         /// <summary>
         /// Test missing API key
         /// </summary>
         [TestMethod]
-        public void PingApiKeyMissingTest()
+        public void GetServerEpochApiKeyMissingTest()
         {
-            ResultEventArgs<PingResult> result = null;
-            IServerService stsyServer = new ServerService(new EtsyContext(string.Empty));
-            stsyServer.PingCompleted += (s, e) => result = e;
+            ResultEventArgs<ServerEpoch> result = null;
+            IServerService etsyServer = new ServerService(new EtsyContext(string.Empty));
+            etsyServer.GetServerEpochCompleted += (s, e) => result = e;
 
             // ACT
-            stsyServer.Ping();
+            etsyServer.GetServerEpoch();
 
             // check the data
             NetsyData.CheckResultFailure(result);
@@ -44,21 +44,21 @@ namespace Netsy.IntegrationTest
         /// Test invalid API key
         /// </summary>
         [TestMethod]
-        public void PingApiKeyInvalidTest()
+        public void GetServerEpochApiKeyInvalidTest()
         {
             // ARRANGE
             using (AutoResetEvent waitEvent = new AutoResetEvent(false))
             {
-                ResultEventArgs<PingResult> result = null;
+                ResultEventArgs<ServerEpoch> result = null;
                 IServerService etsyServer = new ServerService(new EtsyContext("InvalidKey"));
-                etsyServer.PingCompleted += (s, e) =>
+                etsyServer.GetServerEpochCompleted += (s, e) =>
                 {
                     result = e;
                     waitEvent.Set();
                 };
 
                 // ACT
-                etsyServer.Ping();
+                etsyServer.GetServerEpoch();
                 bool signalled = waitEvent.WaitOne(NetsyData.WaitTimeout);
 
                 // ASSERT
@@ -77,21 +77,21 @@ namespace Netsy.IntegrationTest
         /// Test invalid API key
         /// </summary>
         [TestMethod]
-        public void PingCallTest()
+        public void GetServerEpochCallTest()
         {
             // ARRANGE
             using (AutoResetEvent waitEvent = new AutoResetEvent(false))
             {
-                ResultEventArgs<PingResult> result = null;
+                ResultEventArgs<ServerEpoch> result = null;
                 IServerService etsyServer = new ServerService(new EtsyContext(NetsyData.EtsyApiKey));
-                etsyServer.PingCompleted += (s, e) =>
+                etsyServer.GetServerEpochCompleted += (s, e) =>
                 {
                     result = e;
                     waitEvent.Set();
                 };
 
                 // ACT
-                etsyServer.Ping();
+                etsyServer.GetServerEpoch();
                 bool signalled = waitEvent.WaitOne(NetsyData.WaitTimeout);
 
                 // ASSERT
@@ -102,9 +102,13 @@ namespace Netsy.IntegrationTest
                 Assert.IsNotNull(result);
                 NetsyData.CheckResultSuccess(result);
 
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(result.ResultStatus);
+                Assert.IsTrue(result.ResultStatus.Success);
+                Assert.IsNotNull(result.ResultValue);
                 Assert.AreEqual(1, result.ResultValue.Count);
                 Assert.AreEqual(1, result.ResultValue.Results.Length);
-                Assert.AreEqual("pong", result.ResultValue.Results[0]);
+                Assert.IsTrue(result.ResultValue.Results[0] > 0);
                 Assert.IsNull(result.ResultValue.Params);
             }
         }
