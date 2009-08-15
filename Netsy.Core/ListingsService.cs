01aff72434e52197e1d28cb81b390fb1bc9005f0
AnthonySteele <Anthony@PlasticAvalon.org>
2009-08-15 11:28:13 +0100

    started listing service

---
+++ Netsy.Core/ListingsService.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ListingsService.cs" company="AFS">
+//  This source code is part of Netsy http://github.com/AnthonySteele/Netsy/
+//  and is made available under the terms of the Microsoft Public License (Ms-PL)
+//  http://www.opensource.org/licenses/ms-pl.html
+// </copyright>
+//----------------------------------------------------------------------- 
+
+namespace Netsy.Core
+{
+    using DataModel;
+
+    /// <summary>
+    /// Implementation of the listings service
+    /// </summary>
+    public class ListingsService
+    {
+        /// <summary>
+        /// the Etsy context data
+        /// </summary>
+        private readonly EtsyContext etsyContext;
+
+        /// <summary>
+        /// Initializes a new instance of the ListingsService class
+        /// </summary>
+        /// <param name="etsyContext">the etsy context to use</param>
+        public ListingsService(EtsyContext etsyContext)
+        {
+            this.etsyContext = etsyContext;
+        }
+    }
+}
--- Netsy.Core/ServerService.cs
+++ Netsy.Core/ServerService.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright file="IServerService.cs" company="AFS">
+// <copyright file="ServerService.cs" company="AFS">
 //  This source code is part of Netsy http://github.com/AnthonySteele/Netsy/
 //  and is made available under the terms of the Microsoft Public License (Ms-PL)
 //  http://www.opensource.org/licenses/ms-pl.html
