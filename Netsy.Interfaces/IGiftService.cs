4f8f575fe1ff19b9ac9144e252c55cbc17194f0c
unknown <H. Simpson@.(none)>
2009-08-23 11:43:24 +0200

    gift service commented

--- Netsy.Interfaces/IGiftService.cs
+++ Netsy.Interfaces/IGiftService.cs
@@ -10,20 +10,39 @@ namespace Netsy.Interfaces
 {
     using System;
 
-    using DataModel;
-    using DataModel.ListingData;
-
-    using Helpers;
+    using Netsy.DataModel;
+    using Netsy.DataModel.ListingData;
+    using Netsy.Helpers;
 
     /// <summary>
     /// Interface to Gift Guide Commands on the etsy API
     /// </summary>
     public interface IGiftService
     {
+        /// <summary>
+        /// GetGiftGuides completed event
+        /// </summary>
         event EventHandler<ResultEventArgs<Listings>> GetGiftGuidesCompleted;
-        event EventHandler<ResultEventArgs<Listings>> GGetGiftGuideListingsCompleted;
 
-        IAsyncResult GetGiftGuides(int guideId, int offset, int limit, DetailLevel detailLevel);
+        /// <summary>
+        /// GetGiftGuideListings completed event
+        /// </summary>
+        event EventHandler<ResultEventArgs<Listings>> GetGiftGuideListingsCompleted;
+
+        /// <summary>
+        /// Get a list of gift guides.
+        /// </summary>
+        /// <returns>The Async state of the request</returns>
+        IAsyncResult GetGiftGuides();
+
+        /// <summary>
+        /// Get the listings in a gift guide.
+        /// </summary>
+        /// <param name="guideId">Specify the numeric ID of a Gift Guide </param>
+        /// <param name="offset">To page through large result sets, set offset to a multiple of limit</param>
+        /// <param name="limit">Specify the number of results to return</param>
+        /// <param name="detailLevel">Control how much information to return</param>
+        /// <returns>The Async state of the request</returns>
         IAsyncResult GetGiftGuideListings(int guideId, int offset, int limit, DetailLevel detailLevel);
     }
 }
