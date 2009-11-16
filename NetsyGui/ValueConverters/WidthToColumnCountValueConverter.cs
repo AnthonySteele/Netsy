18304f84f86410e9d1bc8cf859d458f191ef6d00
AnthonySteele <Anthony@PlasticAvalon.org>
2009-11-16 22:49:02 +0000

    GUI: dynamic number of columns based on with via a value converter

---
+++ NetsyGui/ValueConverters/WidthToColumnCountValueConverter.cs
@@ -0,0 +1,70 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WidthToColumnCountValueConverter.cs" company="AFS">
+//  This source code is part of Netsy http://github.com/AnthonySteele/Netsy/
+//  and is made available under the terms of the Microsoft Public License (Ms-PL)
+//  http://www.opensource.org/licenses/ms-pl.html
+// </copyright>
+//----------------------------------------------------------------------- 
+
+namespace NetsyGui.ValueConverters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Value converter to determine column count from width
+    /// </summary>
+    public class WidthToColumnCountValueConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        /// <summary>
+        /// Convert width to column count
+        /// </summary>
+        /// <param name="value">the value to convert</param>
+        /// <param name="targetType">the target type</param>
+        /// <param name="parameter">conversion params</param>
+        /// <param name="culture">culture info</param>
+        /// <returns>the converted value</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is double))
+            {
+                return 1;
+            }
+
+            const int DotsPerColumn = 180;
+            const int MaxColumns = 6;
+
+            double doubleValue = (double)value;
+            int count = (int)(doubleValue / DotsPerColumn);
+            if (count < 1)
+            {
+                count = 1;
+            }
+
+            if (count > MaxColumns)
+            {
+                count = MaxColumns;
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Convert back -  not handled
+        /// </summary>
+        /// <param name="value">the value to convert</param>
+        /// <param name="targetType">the target type</param>
+        /// <param name="parameter">conversion params</param>
+        /// <param name="culture">culture info</param>
+        /// <returns>the converted value</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
