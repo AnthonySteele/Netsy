89fef73c9dc2207243b3c2a58f1cc08a1b2f65da
AnthonySteele <Anthony@PlasticAvalon.org>
2009-11-20 23:27:06 +0000

    Added Netsy gui tests project

---
+++ NetsyGui.Test/ViewModels/ListingViewModelTest.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ListingViewModelTest.cs" company="AFS">
+//  This source code is part of Netsy http://github.com/AnthonySteele/Netsy/
+//  and is made available under the terms of the Microsoft Public License (Ms-PL)
+//  http://www.opensource.org/licenses/ms-pl.html
+// </copyright>
+//----------------------------------------------------------------------- 
+
+namespace NetsyGui.Test.ViewModels
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Netsy.DataModel;
+
+    using NetsyGui.ViewModels;
+
+    /// <summary>
+    /// Tests on the listing view models
+    /// </summary>
+    [TestClass]
+    public class ListingViewModelTest
+    {
+        /// <summary>
+        /// Test creating a Listing view model
+        /// </summary>
+        [TestMethod]
+        public void CreateTest()
+        {
+            ListingViewModel viewModel = new ListingViewModel(new Listing());
+            
+            Assert.IsNotNull(viewModel);
+        }
+    }
+}
