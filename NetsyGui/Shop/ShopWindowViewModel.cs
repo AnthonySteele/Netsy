8f48db4da8884d18fb78c8103c8ebd170e87cc37
AnthonySteele <Anthony@PlasticAvalon.org>
2009-11-21 22:53:29 +0800

    STarted on shop window

---
+++ NetsyGui/Shop/ShopWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ShopWindowViewModel.cs" company="AFS">
+//  This source code is part of Netsy http://github.com/AnthonySteele/Netsy/
+//  and is made available under the terms of the Microsoft Public License (Ms-PL)
+//  http://www.opensource.org/licenses/ms-pl.html
+// </copyright>
+//----------------------------------------------------------------------- 
+
+namespace NetsyGui.Shop
+{
+    using ViewModels;
+
+    /// <summary>
+    /// View model for the shop Window
+    /// </summary>
+    public class ShopWindowViewModel : BaseViewModel
+    {
+
+    }
+}
